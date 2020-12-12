4cdbb28199e3e0734fd6f616a92ea7205c77b0e5
Robert <robert.cet@gmail.com>
2020-12-12 14:48:28 +0100

    aktualizacja

--- goldmain/views.py
+++ goldmain/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
-from goldmain.models import GoldPrice
+from goldmain.models import GoldPrice,
 
+
+def main_page(reguest):
+    return render(reguest, 'base.html')
 
 def gold_price_from_website(request):
     URL = 'https://www.mennica.com.pl/produkty-inwestycyjne/analiza-rynku-zlota'
@@ -13,15 +16,8 @@ def gold_price_from_website(request):
         gold_p = currency.find('div', class_='currency').get_text()
     gold_price = float(gold_p.replace('.', '').replace(',', '.').replace(' ', '').replace('USD', ''))
 
-    g = gold_price
-    gold_object = GoldPrice.objects.create(price=g)
+    gold_object = GoldPrice.objects.create(price=gold_price)
     gold_object.save()
 
-    return render(request, "base.html", {"gold_price": gold_price})
-
-
-
-
-
-
+    return render(request, "gold_price.html", {"gold_price": gold_price})
 
